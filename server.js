67fa3cea840a6c9f939f7c74ee55cf302e968c18
Magesh <119650512+bad3767@users.noreply.github.com>
2023-06-30 16:03:13 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+// import express
+const express = require("express");
+const cookieParser=require('cookie-parser')
+const app = express();
+const jwt = require('jsonwebtoken');
+
+// email lib file import
+const nodemailer = require("nodemailer");
+
+// redirect to router
+const userRouter = require("./routes/router");
+
+// database connection
+const mongoose = require("mongoose");
+const data = "mongodb://localhost:27017/email_otp";
+mongoose
+  .connect(data)
+  .then(() => {
+    console.log("connect successfully");
+  })
+  .catch((error) => {
+    console.log("connection failed ", error);
+  });
+
+// for json response
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(cookieParser());
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+
+// URLs for the router
+app.use("/user", userRouter);
+
+// port configuration with env
+const port = process.env.PORT;
+app.listen(port, function () {
+  console.log("localhost:" + port);
+});
